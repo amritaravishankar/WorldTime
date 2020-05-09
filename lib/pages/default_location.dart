4d59b64ddde7f78e1e84c7b7c55a7e2684e26cd5
Amrita <amritaravishankar00@gmail.com>
2020-05-09 16:12:24 +0800

    Merge branch 'master' of /Users/amritaravishankar/Desktop/Flutter/FlutterProjects/WorldTime with conflicts.

--- lib/pages/default_location.dart
+++ lib/pages/default_location.dart
@@ -18,31 +18,33 @@ class _DefaultState extends State<Default> {
 
 class Def extends StatefulWidget {
 
+  final List<WorldTime> locationList = ChooseLocation.locations;
+  final List<int> defaultCheck = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+  final String defaultLoc = "Mumbai";
+
+  Def({Key key}):super(key:key);
+
   @override
   _DefState createState() => _DefState();
 }
 
 class _DefState extends State<Def> {
-  final List<WorldTime> locationList = ChooseLocation.locations;
-  List<int> defaultCheck = [0,0,0,0,0,0,0,0,1,0,0,0,0,0];
 
-  String defaultLoc = 'Mumbai';
+  String defaultLoc = "Mumbai";
 
-  void updateDefault(index){
+  void updateDefault(index) {
     print('Option highlighted');
-    defaultLoc = locationList[index].location;
-    for(int i = 0; i < defaultCheck.length; i++) {
-
-      if (defaultCheck[i]==1) {
-      defaultCheck[i] -= 1;
+    defaultLoc = widget.locationList[index].location;
+    for (int i = 0; i < widget.defaultCheck.length; i++) {
+      if (widget.defaultCheck[i] == 1) {
+        widget.defaultCheck[i] -= 1;
       }
     }
-    defaultCheck[index]=1;
+    widget.defaultCheck[index] = 1;
   }
-  void removeDefault(index)
-  {
-    defaultCheck[index]=0;
-    //if all values are 0, display "no default location selected! Please select one!" or give out an Alert message
+
+  void removeDefault(index) {
+    widget.defaultCheck[index] = 0; //if all values are 0, display "no default location selected! Please select one!" or give out an Alert message
   }
 
   @override
@@ -55,10 +57,11 @@ class _DefState extends State<Def> {
       ),
       body: Column(
         children: <Widget>[
-          SizedBox(height:75.0, child: Card(
+          SizedBox(height: 75.0, child: Card(
               color: Colors.yellow[100],
               child: Center(child: Text(defaultLoc,
-              style: TextStyle( fontSize: 20.0, fontWeight: FontWeight.w500))))),
+                  style: TextStyle(
+                      fontSize: 20.0, fontWeight: FontWeight.w500))))),
           SizedBox(
             height: 510.0,
             child: ListView.builder(
@@ -66,19 +69,26 @@ class _DefState extends State<Def> {
                 itemCount: 14,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 1.0, horizontal: 4.0),
                     child: Card(
                       child: ListTile(
-                        title: Text(locationList[index].location),
+                        title: Text(widget.locationList[index].location),
                         leading: CircleAvatar(
-                          backgroundImage: AssetImage('assets/${locationList[index].flag}'),
+                          backgroundImage: AssetImage(
+                              'assets/${widget.locationList[index].flag}'),
                         ),
                         onTap: () {
                           setState(() {
-                            defaultCheck[index] == 0? updateDefault(index): removeDefault(index);
+                            widget.defaultCheck[index] == 0
+                                ? updateDefault(index)
+                                : removeDefault(index);
+                            //storeIndex = index;
                           });
                         },
-                        trailing: defaultCheck[index]==1? Icon(Icons.check): Text(''),
+                        trailing: widget.defaultCheck[index] == 1
+                            ? Icon(Icons.check)
+                            : Text(''),
                       ),
                     ),
                   );
